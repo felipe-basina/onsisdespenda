4a5e0a98058af507ad44243acd6176fa05bcc53e
felipe-basina <felipe.basina@gmail.com>
2022-08-18 23:12:03 -0300

    Refatoracao para utilizar funcao de retorno.

--- sisdespenda/views/inicial.py
+++ sisdespenda/views/inicial.py
@@ -1,23 +1,16 @@
 # -*- coding: utf-8 -*-
-from django.http import *
-from django.shortcuts import render, get_object_or_404, render_to_response, redirect
-from django.template import RequestContext
-from django.utils import timezone
-from django.utils.timezone import utc
-import datetime
-from . import evento
-from django.http import HttpResponseRedirect, HttpResponseNotFound
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import auth
+
 
 def pagina_nao_encontrada(request):
-    return render_to_response('404.html')
+    return render(request, '404.html')
+
 
 @login_required(login_url='/accounts/login/')
 def inicio(request):
     '''
     evento.evento_new(request, 'LOGIN USUÁRIO')
     '''
-    mensagem = 'Olá %s, seja bem-vindo(a)!' % (request.user)
+    mensagem = 'Olá %s, seja bem-vindo(a)!' % request.user
     return render(request, 'onsis/inicio.html', {'mensagem': mensagem, 'status': 'info'})
