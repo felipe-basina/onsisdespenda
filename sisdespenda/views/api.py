979b6eac75d008eebf1d3f2c5eb8c94b4af5b296
felipe-basina <felipe.basina@gmail.com>
2023-06-19 22:39:03 -0300

    Adicionado decorator para remover controle csrf.

--- sisdespenda/views/api.py
+++ sisdespenda/views/api.py
@@ -1,3 +1,4 @@
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
@@ -6,25 +7,27 @@ from django.db import connection
 
 
 @api_view(['POST'])
+@csrf_exempt
 def despesa_add(request):
     content = request.data
     print('Despesa >>> {}'.format(content))
     save_data(
         'INSERT INTO despesa_tbl (cd_usuario, dt_despesa, vl_despesa, cd_tipo_despesa, ds_despesa, dt_criacao, '
         'dt_atualizacao) VALUES (%s, %s, %s, %s, %s, now(), now())',
-        (content['cd_usuario'], content['dt_despesa'], content['vl_despesa'], 7, content['ds_despesa'])
+        (content['cd_usuario'], content['dt_despesa'], float("{:.2f}".format(content['vl_despesa'])), 7, content['ds_despesa'])
     )
     return Response(data=request.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['POST'])
+@csrf_exempt
 def renda_add(request):
     content = request.data
     print('Renda >>> {}'.format(content))
     save_data(
         'INSERT INTO renda_tbl (cd_usuario, dt_renda, vl_renda, cd_tipo_renda, dt_criacao, dt_atualizacao) '
         'VALUES (%s, %s, %s, %s, now(), now())',
-        (content['cd_usuario'], content['dt_renda'], content['vl_renda'], 3)
+        (content['cd_usuario'], content['dt_renda'], float("{:.2f}".format(content['vl_renda'])), 3)
     )
     return Response(data=request.data, status=status.HTTP_201_CREATED)
 
